e04897fa5efd2147e2287104ba4ff60e3337df68
SeoHyeon <shyeon16@ewha.ac.kr>
2025-11-03 05:24:06 +0900

    Create MemberForm.java

---
+++ section6/MemberForm.java
@@ -0,0 +1,11 @@
+//hello/hello_spring/controller/MemberForm
+ package hello.hellospring.controller;
+ public class MemberForm {
+ private String name;
+ public String getName() {
+ return name;
+    }
+ public void setName(String name) {
+ this.name = name;
+    }
+ }
