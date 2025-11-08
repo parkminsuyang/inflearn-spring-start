912650f6634fda1f329f6b25136e5e6bf7860a3f
SeoHyeon <shyeon16@ewha.ac.kr>
2025-11-08 23:03:08 +0900

    Create SpringConfig_5.java

---
+++ section7/SpringConfig_5.java
@@ -0,0 +1,28 @@
+//hello/hello_spring/service/SpringConfig
+
+package hello.hello_spring.service;
+
+import hello.hello_spring.repository.*;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository);
+    }
+
+}
